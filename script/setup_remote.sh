4a7b46a915967bdeedc86426e8eba34c251f3b4c
giongto35 <giongto35@yahoo.com>
2020-12-21 02:21:42 +0800

    Update interactive-wine

--- script/setup_remote.sh
+++ script/setup_remote.sh
@@ -3,16 +3,15 @@ ssh root@$1 "cd $RPATH;git clone -b Fix-chore https://github.com/giongto35/cloud
 rsync ./config.yaml root@$1:cloud-morph/
 ssh root@$1 "cd $RPATH/cloud-morph; ./setup.sh"
 rsync $2 root@$1:$RPATH/cloud-morph/winvm/apps
-#ssh root@$1 "docker run -v winecfg:/root/.wine -v $(pwd):/backup --name winebackup syncwine tar cvf /backup/backup.tar /root/.wine"
 
 # set flag
 
 if [ "$3" == "syncvolume" ]; then
     # if sync volume copy wine environment from local to server
     docker rm winebackup -f | true
-    docker run -v winecfg:/root/.wine -v $(pwd):/backup --name winebackup syncwine tar cvf /backup/backup.tar /root/.wine
-    scp backup.tar root@$1:/root
-    ssh root@$1 "docker run -v $(pwd):/backup --volume winecfg:/root/.wine syncwine bash -c \"tar xvf /backup/backup.tar -C /root --strip 1\""
+    docker run -v winecfg:$RPATH/.wine -v $(pwd):/backup --name winebackup syncwine tar cvf /backup/backup.tar $RPATH/.wine
+    scp backup.tar root@$1:$RPATH
+    ssh root@$1 "docker run -v $RPATH:/backup --volume winecfg:$RPATH/.wine syncwine bash -c \"tar xvf /backup/backup.tar -C $RPATH --strip 1\""
 fi
 
 # run server
