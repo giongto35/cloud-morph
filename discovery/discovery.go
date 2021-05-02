2547814a511f234b774906fbc34962fb93327b63
giongto35 <giongto35@yahoo.com>
2021-05-02 13:04:38 +0800

    Fix discovery to skip private

--- discovery/discovery.go
+++ discovery/discovery.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gofrs/uuid"
@@ -238,7 +239,7 @@ func isPrivateIP(ip net.IP) bool {
 func (s *server) register(w http.ResponseWriter, r *http.Request) {
 	var h appDiscoveryMeta
 
-	log.Println("Received Register Request")
+	log.Println("Received Register Request", h)
 	// Try to decode the request body into the struct. If there is an error,
 	// respond to the client with the error message and a 400 status code.
 	err := json.NewDecoder(r.Body).Decode(&h)
@@ -246,7 +247,9 @@ func (s *server) register(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	if isPrivateIP(net.ParseIP(h.Addr)) {
+	ipAddr := strings.Split(h.Addr, ":")[0]
+	if isPrivateIP(net.ParseIP(ipAddr)) {
+		log.Println("Skip Private IP", h.Addr)
 		return
 	}
 	appID, err := s.discovery.addApp(h)
