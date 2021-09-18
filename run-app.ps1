33e99bfb45a2525ea8aa9abe6cba0c4f5800f55b
giongto35 <thanhnguyen17101994@gmail.com>
2021-09-19 03:47:45 +0800

    Fix window run

--- run-app.ps1
+++ run-app.ps1
@@ -1,14 +1,15 @@
-taskkill /FI "ImageName eq notepad.exe" /F
+$path = $args[0]
+$appfile = $args[1]
+
+taskkill /FI "ImageName eq $appfile" /F
 taskkill /FI "ImageName eq ffmpeg.exe" /F
-$app = Start-Process notepad -PassThru
+$app = Start-Process "winvm/$path/$appfile" -PassThru
 sleep 2
-echo $app.id
-echo (Get-Process -Id $app.id)
-echo (Get-Process -Id $app.id).mainWindowTitle
 $title = ((Get-Process -Id $app.id).mainWindowTitle)
-echo "Title"$title
 sleep 2
+echo "title$title"
 Start-Process ffmpeg -PassThru -ArgumentList "-f gdigrab -framerate 30 -i title=`"$title`" -pix_fmt yuv420p -vf scale=1280:-2 -c:v libvpx -f rtp rtp://127.0.0.2:5004"
 sleep 2
+Start-Process syncinput.exe -PassThru -ArgumentList $title
 
 echo "Done"
