168d3f39d5628ea7b9d2cb3b103dcbc8499d734f
giongto35 <thanhnguyen17101994@gmail.com>
2021-09-28 00:49:51 +0800

    Update

--- run-app.ps1
+++ run-app.ps1
@@ -11,7 +11,7 @@ sleep 2
 $title = ((Get-Process -Id $app.id).mainWindowTitle)
 sleep 2
 Start-Process ffmpeg -PassThru -ArgumentList "-f gdigrab -framerate 30 -i title=`"$title`" -pix_fmt yuv420p -vf scale=1280:-2 -c:v libvpx -f rtp rtp://127.0.0.2:5004"
-lleep 2
+sleep 2
 x86_64-w64-mingw32-g++ .\winvm\syncinput.cpp -o .\winvm\syncinput.exe -lws2_32 -lpthread -static
 
 Start-Process winvm/syncinput.exe -PassThru -ArgumentList "$title", ".", "windows"
