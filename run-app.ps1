91e929ef53b6797ffbaeedc374712025bc492bb5
giongto35 <thanhnguyen17101994@gmail.com>
2021-10-03 16:05:19 +0800

    Update format

--- run-app.ps1
+++ run-app.ps1
@@ -13,7 +13,7 @@ $title = ((Get-Process -Id $app.id).mainWindowTitle)
 sleep 2
 if ($isSandbox -eq "sandbox") {
     $localEthernetIP = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias ethernet).IPAddress
-    Start-Process $PSScriptRoot/winvm/pkg/ffmpeg.exe -PassThru -ArgumentList "-f gdigrab -framerate 30 -i title=`"$title`" -pix_fmt yuv420p -vf scale=1280:-2 -c:v libvpx -f rtp rtp://$(localEthernetIP):5004"
+    Start-Process $PSScriptRoot/winvm/pkg/ffmpeg.exe -PassThru -ArgumentList "-f gdigrab -framerate 30 -i title=`"$title`" -pix_fmt yuv420p -vf scale=1280:-2 -c:v libvpx -f rtp rtp://$localEthernetIP`:5004"
 }
 else {
     Start-Process ffmpeg -PassThru -ArgumentList "-f gdigrab -framerate 30 -i title=`"$title`" -pix_fmt yuv420p -vf scale=1280:-2 -c:v libvpx -f rtp rtp://127.0.0.1:5004"
