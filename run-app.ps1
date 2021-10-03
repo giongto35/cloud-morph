835558401f50d1320cc7c557011093e05f0741b1
giongto35 <thanhnguyen17101994@gmail.com>
2021-10-03 15:48:51 +0800

    Turn off fire wall

--- run-app.ps1
+++ run-app.ps1
@@ -12,12 +12,13 @@ sleep 2
 $title = ((Get-Process -Id $app.id).mainWindowTitle)
 sleep 2
 if ($isSandbox -eq "sandbox") {
+    Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
     Start-Process $PSScriptRoot/winvm/pkg/ffmpeg.exe -PassThru -ArgumentList "-f gdigrab -framerate 30 -i title=`"$title`" -pix_fmt yuv420p -vf scale=1280:-2 -c:v libvpx -f rtp rtp://127.0.0.1:5004"
 }
 else {
     Start-Process ffmpeg -PassThru -ArgumentList "-f gdigrab -framerate 30 -i title=`"$title`" -pix_fmt yuv420p -vf scale=1280:-2 -c:v libvpx -f rtp rtp://127.0.0.1:5004"
 }
 sleep 2
-# x86_64-w64-mingw32-g++ $PSScriptRoot\winvm\syncinput.cpp -o $PSScriptRoot\winvm\syncinput.exe -lws2_32 -lpthread -static
+x86_64-w64-mingw32-g++ $PSScriptRoot\winvm\syncinput.cpp -o $PSScriptRoot\winvm\syncinput.exe -lws2_32 -lpthread -static
 
 Start-Process $PSScriptRoot/winvm/syncinput.exe -PassThru -ArgumentList "$title", ".", "windows"
