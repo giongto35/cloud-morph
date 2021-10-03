64e97ac6d4dc310739cd4d93757855342858cacb
giongto35 <thanhnguyen17101994@gmail.com>
2021-10-03 13:43:02 +0800

    Update run sandbox

--- run-sandbox.ps1
+++ run-sandbox.ps1
@@ -14,9 +14,9 @@ $template = @'
 </Configuration>
 '@
 # Init Sandbox mount dir
-mkdir winvm/pkg
-Invoke-WebRequest -Uri "https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z" -OutFile winvm/pkg/ffmpeg.7z
-7z e winvm/pkg/ffmpeg.7z  winvm/pkg/
+# mkdir winvm/pkg
+# Invoke-WebRequest -Uri "https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z" -OutFile winvm/pkg/ffmpeg.7z
+# 7z e winvm/pkg/ffmpeg.7z  winvm/pkg/
 
 # Create Sandbox Config
 $template -f $args[0], $args[1], "$PWD"  | Out-File -FilePath .\run-sandbox.wsb
