fb7f9444fba679f9390be5f5a292a9da730778c0
giongto35 <thanhnguyen17101994@gmail.com>
2021-10-03 13:17:31 +0800

    run app

--- run-app.ps1
+++ run-app.ps1
@@ -1,5 +1,6 @@
 $path = $args[0]
 $appfile = $args[1]
+$isSandbox = $args[2]
 # Split-Path $outputPath -leaf
 echo "running $PSScriptRoot/winvm/$path/$appfile"
 
@@ -10,7 +11,12 @@ $app = Start-Process "$PSScriptRoot/winvm/$path/$appfile" -PassThru
 sleep 2
 $title = ((Get-Process -Id $app.id).mainWindowTitle)
 sleep 2
-Start-Process ffmpeg -PassThru -ArgumentList "-f gdigrab -framerate 30 -i title=`"$title`" -pix_fmt yuv420p -vf scale=1280:-2 -c:v libvpx -f rtp rtp://127.0.0.1:5004"
+if ($isSandbox = "sandbox") {
+    Start-Process $PSScriptRoot/winvm/pkg/ffmpeg.exe -PassThru -ArgumentList "-f gdigrab -framerate 30 -i title=`"$title`" -pix_fmt yuv420p -vf scale=1280:-2 -c:v libvpx -f rtp rtp://127.0.0.1:5004"
+}
+else {
+    Start-Process ffmpeg -PassThru -ArgumentList "-f gdigrab -framerate 30 -i title=`"$title`" -pix_fmt yuv420p -vf scale=1280:-2 -c:v libvpx -f rtp rtp://127.0.0.1:5004"
+}
 sleep 2
 # x86_64-w64-mingw32-g++ $PSScriptRoot\winvm\syncinput.cpp -o $PSScriptRoot\winvm\syncinput.exe -lws2_32 -lpthread -static
 
