784d59fdae4a8b9d9392e1634969226d9053075a
giongto35 <thanhnguyen17101994@gmail.com>
2021-10-09 17:44:42 +0800

    CHange winevm to winvm for less confusion

--- winvm/test.sh
+++ winvm/test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-x86_64-w64-mingw32-g++ ./syncinput.cpp -o /winevm/syncinput.exe -lws2_32 -static-libgcc -static-libstdc++ 
+x86_64-w64-mingw32-g++ ./syncinput.cpp -o /winvm/syncinput.exe -lws2_32 -static-libgcc -static-libstdc++ 
