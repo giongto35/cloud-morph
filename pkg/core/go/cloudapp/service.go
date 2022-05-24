099355c6a0821759b4058044bbef4bbe63e45151
sergystepanov <sergystepanov@gmail.com>
2022-05-24 19:34:03 +0300

    Remove unused code

--- pkg/core/go/cloudapp/service.go
+++ pkg/core/go/cloudapp/service.go
@@ -1,7 +1,6 @@
 package cloudapp
 
 import (
-	"encoding/base64"
 	"encoding/json"
 	"log"
 	"sync"
@@ -10,7 +9,6 @@ import (
 	"github.com/giongto35/cloud-morph/pkg/common/config"
 	"github.com/giongto35/cloud-morph/pkg/common/cws"
 	"github.com/giongto35/cloud-morph/pkg/core/go/cloudapp/webrtc"
-
 	"github.com/pion/rtp"
 )
 
@@ -317,29 +315,4 @@ func (s *Service) Handle() {
 		}
 	}()
 	s.ccApp.Handle()
-}
-
-// Encode encodes the input in base64
-// It can optionally zip the input before encoding
-func Encode(obj interface{}) string {
-	b, err := json.Marshal(obj)
-	if err != nil {
-		panic(err)
-	}
-
-	return base64.StdEncoding.EncodeToString(b)
-}
-
-// Decode decodes the input from base64
-// It can optionally unzip the input after decoding
-func Decode(in string, obj interface{}) {
-	b, err := base64.StdEncoding.DecodeString(in)
-	if err != nil {
-		panic(err)
-	}
-
-	err = json.Unmarshal(b, obj)
-	if err != nil {
-		panic(err)
-	}
 }
